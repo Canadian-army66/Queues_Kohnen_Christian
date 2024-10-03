f68435a2c94d934cecb760e4d5a37a533177f3ae
I Eat Babies <christiankohnen50@gmail.com>
2024-10-03 10:42:33 -0500

    hjkl

--- Queues_Kohnen_Christian/Program.cs
+++ Queues_Kohnen_Christian/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Queues_Kohnen_Christian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Poopybutthole");
+        }
+    }
+}
