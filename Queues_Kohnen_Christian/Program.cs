ae4297b12496034ca5debfae6e739f62e35bec69
I Eat Babies <christiankohnen50@gmail.com>
2024-10-07 10:13:16 -0500

    comments x2

--- Queues_Kohnen_Christian/Program.cs
+++ Queues_Kohnen_Christian/Program.cs
@@ -6,31 +6,33 @@ namespace Queues_Kohnen_Christian
     {
         static void Main(string[] args)
         {
-            Games myGame = new Games(50, 49);
-            Queue<Player> waitinfToJoinQ = new Queue<Player>();
-
+            Games myGame = new Games(50, 49);                       //creates a game object that has a max player count of 50 and the current players 49
+            Queue<Player> waitinfToJoinQ = new Queue<Player>();     //creates a queue for player objects
+            
+            //creates player objects
             Player p1 = new Player(100, "XX_BrickGamer_XX");
             Player p2 = new Player(100, "GamerZ67");
             Player p3 = new Player(100, "BombasticLion_6752");
             Player p4 = new Player(100, "Mike2001");
 
+            //calls JoinGame for the players
             p1.JoinGame(myGame, waitinfToJoinQ);
             p2.JoinGame(myGame, waitinfToJoinQ);
             p3.JoinGame(myGame, waitinfToJoinQ);
             p4.JoinGame(myGame, waitinfToJoinQ);
 
-            Console.WriteLine("Players in the queue:");
-            foreach(Player player in waitinfToJoinQ)
+            Console.WriteLine("Players in the queue:");             //prints title for the queue
+            foreach(Player player in waitinfToJoinQ)                //for each player in the queue, it prints their gamertag
             {
                 Console.WriteLine(player);
             }
 
-            myGame.KickPlayer();
+            myGame.KickPlayer();                                    //calls KickPlayer method
 
-            myGame.CheckQueue(waitinfToJoinQ);
+            myGame.CheckQueue(waitinfToJoinQ);                      //checks the queue
 
-            Console.WriteLine("\nPlayers in the queue:");
-            foreach (Player player in waitinfToJoinQ)
+            Console.WriteLine("\nPlayers in the queue:");           //prints title for the queue
+            foreach (Player player in waitinfToJoinQ)               //for each player in the queue, it prints their gamertag
             {
                 Console.WriteLine(player);
             }
